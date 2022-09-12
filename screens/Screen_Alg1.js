5e736e68b4726c7a648b9ef954d39ae0247bf2d5
Marcia Lechuga López <marcia.lechuga25@gmail.com>
2022-09-12 12:41:53 -0500

    screen_prueba
    prueba de screen_alg

---
+++ screens/Screen_Alg1.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import { useNavigation} from '@react-navigation/native';
+
+
+export default function Screen_Alg1() {
+  return (
+    <SafeAreaView className='bg-gray-400'>
+      <Text className='font-bold text-gray-300 text-lg'>Home</Text>
+    </SafeAreaView>
+  )
+}
