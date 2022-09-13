4a069a9462114e046e77a6a617d19d18458d317c
Marcia Lechuga López <39806327+marchilops@users.noreply.github.com>
2022-09-13 10:47:25 -0500

    modificación de screens 1 y 2

--- screens/Screen_Alg2.js
+++ screens/Screen_Alg2.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native'
+import { View, Text, ScrollView, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 
@@ -9,7 +9,7 @@ const navigation = useNavigation()
   return (
     <>
     <SafeAreaView className='bg-gray-400'>
-      <Text className='font-bold text-gray-300 text-lg font-light text-xl text-center p-2'>Algorithm 2</Text>
+      <Text className='font-bold text-gray-300 text-lg font-light text-xl text-center p-2'>Algorithm 2 - KNN</Text>
     </SafeAreaView>
 
     <ScrollView>
@@ -20,7 +20,7 @@ const navigation = useNavigation()
             </View>
             <View className='flex-column px-2'>
             <Text>&emsp;</Text>
-              <Text className='text mb-1'>**Poner nuestra justificación** </Text>
+              <Text className='text mb-1'>We choose this algorithm because after analyzing...</Text>
             </View>
           </View>
         </View>
@@ -30,6 +30,10 @@ const navigation = useNavigation()
             <View className='pl-2 pb-3 pt-0 '>
               <Text>&emsp;</Text>
               <Text className="font-bold text-xl">Precision obtained</Text>
+            </View>
+            <View className='flex-column px-2'>
+            <Text>&emsp;</Text>
+              <Text className='text mb-1'>0.7877</Text>
             </View>
           </View>
         </View>
@@ -41,6 +45,10 @@ const navigation = useNavigation()
               <Text>&emsp;</Text>
               <Text className="font-bold text-xl">Score of Kaggle</Text>
             </View>
+            <View className='flex-column px-2'>
+            <Text>&emsp;</Text>
+              <Text className='text mb-1'>0.70813</Text>
+            </View>
           </View>
         </View>
         
@@ -49,6 +57,10 @@ const navigation = useNavigation()
             <View className='pl-2 pb-3 pt-0 '>
               <Text>&emsp;</Text>
               <Text className="font-bold text-xl">Confusion matrix</Text>
+              <Image
+              source={ require('../assets/imgs/matrixknn.png') }
+              className="h-10 w-10"
+              />
             </View>
           </View>
         </View>
@@ -57,16 +69,11 @@ const navigation = useNavigation()
           <View> 
             <View className='pl-2 pb-3 pt-0 '>
               <Text>&emsp;</Text>
-              <Text className="font-bold text-xl">ROC</Text>
-            </View>
-          </View>
-        </View>
-
-        <View className='bg-blue pl-2 border-gray-200'>
-          <View> 
-            <View className='pl-2 pb-3 pt-0 '>
-              <Text>&emsp;</Text>
-              <Text className="font-bold text-xl">AUC</Text>
+              <Text className="font-bold text-xl">ROC and AUC</Text>
+              <Image
+              source={ require('../assets/imgs/auckn.png') }
+              className="h-10 w-10"
+              />
             </View>
           </View>
         </View>
@@ -74,3 +81,4 @@ const navigation = useNavigation()
     </>
   )
 }
+
